e640bc939992cca0036cea4efaf0535326719108
majadev <mgedda@live.se>
2017-05-06 15:44:32 +0200

    Squashed commit of the following: show the actvities

--- script.js
+++ script.js
@@ -1,1 +1,44 @@
-console.log('hejjejerff');
+console.log('hejhej');
+
+
+$(document).ready(function(){
+        $.ajax({url: "http://fritida.se/api/activities/", success: function(result){
+          console.log(result);
+          $.each(result, function( index, activity ) {
+            console.log( index );
+            console.log(activity.title);
+            console.log(activity["description"]);
+            $("#activities").append('<li>' + activity.title + '<br>'  + activity.description + '</li>' );
+          });
+        }});
+});
+
+
+
+
+//--------Menu stuff----------
+var $select = $("<select></select>");
+  $("#menu").append($select);
+
+  //Cycle over menu links
+  $("#menu a").each(function(){
+    var $anchor = $(this);
+    //Create an option
+    var $option = $("<option></option>");
+
+    if($anchor.parent().hasClass("selected")){
+        $option.prop("selected", true);
+    };
+      //Option's value is the href value of the links
+      $option.val($anchor.attr("href"));
+      //option's text is the text of links
+      $option.text($anchor.text());
+      //append option to select
+      $select.append($option);
+  });
+
+    //Bind click to button
+    $select.change(function(){
+      // go to select's location
+    window.location = $select.val();
+    });
