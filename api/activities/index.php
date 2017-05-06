e566f3b41aa803bd6a5eda307171236dc034aa14
Massagemotorn <you@example.com>
2017-05-06 11:45:15 +0200

    wip

--- api/activities/index.php
+++ api/activities/index.php
@@ -1,4 +1,23 @@
 <?php
+  $json = file_get_contents('php://input');
+  $obj = json_decode($json);
+  var_dump($json);
+  var_dump($obj);
   include 'secret.php';
-  var_dump($_POST);
+
+  $mysqli = new mysqli($databaseUrl, $databaseUser, $databasePassword, $databaseName);
+
+  if ($result = $mysqli->query("SELECT * FROM activities")) {
+    $output = "[\n";
+    while($activity = $result->fetch_assoc()) {
+      $output .= "  {\n";
+      $output .= '    "title":"'.$activity["title"].'",';
+      $output .= '    "title":"'.$activity["description"].'"';
+      $output .= "\n  },";
+    }
+    $output .= "\n]";
+    $output = str_replace(",\n]", "\n]", $output);
+    echo $output;
+    $result->close();
+}
 ?>
